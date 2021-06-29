ccebcb4a13c6bd1097822eccfe39f9237f1b57b9
wandraded <wandraded4@gmail.com>
2021-06-29 19:13:33 +0200

    mi primer java

---
+++ week-1/InClass/exercise-B.js
@@ -0,0 +1,5 @@
+console.log("Maria tiene un lorito")
+console.log("maria tiene un perrito")
+console.log("Maria tiene un gatico")
+console.log("Maria tiene muchas animalitos")
+console.log("Maria es feliz")
