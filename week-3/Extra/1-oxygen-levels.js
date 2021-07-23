7d4dde7302d33d4d26dbadcb4b1ddead444cff02
wandraded <wandraded4@gmail.com>
2021-07-23 11:25:56 +0200

    Coreeccion tarea h1

--- week-3/Extra/1-oxygen-levels.js
+++ week-3/Extra/1-oxygen-levels.js
@@ -9,8 +9,8 @@ To be safe to land on, a planet needs to have an Oxygen level between 19.5% and 
 Write a function that finds the first safe oxygen level in the array - Oxygen between 19.5% and 23.5%
 */
 
-function safeLevels() {
-
+function safeLevels(oxygenLevels) {
+return oxygenLevelsRaw.map(Level >= parseFloat(level.slice(0,-1))).find() && level>=23.5)
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
